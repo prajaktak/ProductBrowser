6bed7b93618ef26ff0e3676f3abb8b00f90b2b6d
Prajakta Kulkarni <prachee.j@gmnail.com>
2018-07-01 22:38:02 +0200

    integrated webservice manager class, also integrated code to save records in persistent database. implemented SSL pinning.

--- ProductBrowser/ProductListingViewController.swift
+++ ProductBrowser/ProductListingViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
-class ProductListingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
-    
+class ProductListingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var productsTableView: UITableView!
-    var productsArray:NSArray!
+    var productsArray : [Product]!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // swiftlint:disable line_length
+        WebserviceManager().getData(form: "https://gist.githubusercontent.com/anonymous/a3b3e50413fff111505a/raw/0522419f508e7ea506a8856586dce11a5664e9df/products.json") { (result) in
+            switch result{
+            case .success(let jsonObject):
+                CoreDataManager.sharedInstance.saveInCoreDataWith(array: jsonObject)
+                print("Success")
+            case .failure:
+                print("Failure")
+            }
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -22,23 +31,20 @@ class ProductListingViewController: UIViewController, UITableViewDelegate, UITab
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    //MARK - TableView delegate and datasource methods
+    // MARK: - TableView delegate and datasource methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if productsArray != nil {
             return productsArray.count
         }
         return 0
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let productCell:ProductListTableViewCell =  tableView.dequeueReusableCell(withIdentifier: "productCell") as! ProductListTableViewCell
-        productCell.imageView?.image = UIImage(named: "")
-        productCell.productTitleLabel.text =  "productTitle"
+        // swiftlint:disable force_cast line_length
+        let productCell: ProductListTableViewCell =  tableView.dequeueReusableCell(withIdentifier: "productCell") as! ProductListTableViewCell
+        // swiftlint:enable force_cast line_length
+        productCell.imageView?.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        productCell.productTitleLabel.text = (productsArray[indexPath.row] as [String: AnyObject])
         return productCell
     }
 
-
-
 }
-
